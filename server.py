e7972b7202885b8da46f5d19be4d8fa05f955329
Colin Rosenthal <crosenthal25@seaburyhall.org>
2021-11-30 20:27:45 +0000

    Initial Commit

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+projects = [{"name": "Airplane", "description": "We build airplanes", "portrait": "/static/airplane.png"},
+            {"name": "Car", "description": "We build cars", "portrait": "/static/car.png"},
+            {"name": "3D Printer", "description": "We build 3D printers", "portrait": "/static/printer.png"},
+            {"name": "DIY", "description": "We build DIY solar panels", "portrait": "/static/panel.png"}]
+
+links = ["/projects", "https://youtube.com"]
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.route("/projects")
+def get_projects():
+
+    return render_template('projects.html', projects=projects)
